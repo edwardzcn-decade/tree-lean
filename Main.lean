54e81f6a08215517d169ba94b1fd4a8a6b616179
edwardzcn-decade <edwardzcn98@gmail.com>
2024-12-05 13:51:26 +0800

    fix: main and config

--- Main.lean
+++ Main.lean
@@ -1,15 +1,15 @@
 import TreeLean.Config
 open System
 
-
+def default_layer : Nat := 3
 
 def printUsage : IO Unit := do
-  IO.println (fancy "Usage: treelean [-a] [--ascii] [--help] [--dir <path>] [--version]\nOptions:\n\t--ascii\tUse ASCII characters to display the directory structure")
+  IO.println (FancyConfig.fancyF "Usage: treelean [-a] [--ascii] [--help] [--dir <path>] [--version]\nOptions:\n\t--ascii\tUse ASCII characters to display the directory structure")
 
 def printVersion : IO Unit := do
-  IO.println (fancy "treelean 0.1.0")
+  IO.println (FancyConfig.fancyF "treelean 0.1.0")
 
-def configFromArgs : List String → IO (Option Config)
+def configFromArgs : List String → IO (Option BasicConfig)
   | [] => pure none
   | args =>
     if (args.contains "--help" || args.contains "-h") then do
@@ -20,12 +20,18 @@ def configFromArgs : List String → IO (Option Config)
       printVersion
       pure none
     else
+      do
       let ascii := args.contains "--ascii"
       let fancy := args.contains "--fancy"
       let argslen := args.length
       let id_path := args.indexOf "--dir" + 1
-      let rootpath := if argslen > 0  && id_path < argslen then args[id_path]! else "/"
-      pure (some { ascii := ascii, fancy := fancy, rootpath := rootpath })
+      let root_path := if argslen > 0  && id_path < argslen then args[id_path]! else "/"
+
+      let id_max_layer := args.indexOf "--layer" + 1
+      -- TODO add Nat Check
+      let max_layer : Nat := if argslen > 0 && id_max_layer < argslen then args[id_max_layer]!.toNat! else default_layer
+      -- println! s!"max_layer: {max_layer}"
+      pure (some { ascii := ascii, fancy := fancy, rootPath := root_path, maxLayer := some max_layer })
 
 
 
@@ -79,7 +85,8 @@ def main (args : List String) : IO UInt32 := do
     IO.eprintln s!"# Read Usage: treelean --help"
     pure 1
   | some config =>
-    let showConfig := ShowConfig.fromConfig config
-    println! s!"root path: {config.rootpath}"
-    dirTree showConfig config.rootpath 1 *>
+    let showConfig := ShowConfig.fromBasicConfig config
+    println! s!"root path: {showConfig.rootPath}"
+    -- TODO add default max_layer error handling
+    dirTree showConfig showConfig.rootPath config.maxLayer.get! *>
     pure 0
